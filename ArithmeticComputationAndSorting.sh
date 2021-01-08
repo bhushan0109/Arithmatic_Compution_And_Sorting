9d60cdc781428c47aa5007835966ca7184a054c4
bhushan_patil <bhushan>
2021-01-08 23:05:30 +0530

    store the result in dictionary

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -6,3 +6,8 @@ result1=$(($a + $b * $c ))
 result2=$(($(( $a * $b )) + $c ))
 result3=$(( $c + $(( $a / $b )) ))
 result4=$(($(( $a % $b )) + $c ))
+#TO STORE THE DICTIONARY
+arithmaticOperation[result1]=$result1
+arithmaticOperation[result2]=$result2
+arithmaticOperation[result3]=$result3
+arithmaticOperation[result4]=$result4
