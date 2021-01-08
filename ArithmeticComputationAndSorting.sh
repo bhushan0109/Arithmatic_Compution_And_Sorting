7250e24fba8df4af4093b6ecdc0514f1e9c342c5
bhushan_patil <bhushan>
2021-01-08 23:10:20 +0530

    read the value dict to array

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -x
+
+#TO DECLEAR THE DICTIONARY
+declare -A arithmaticOperation
+
 read -p "Enter first input :" a
 read -p "Enter second input:" b
 read -p "Enter third input :" c
+
 result1=$(($a + $b * $c ))
 result2=$(($(( $a * $b )) + $c ))
 result3=$(( $c + $(( $a / $b )) ))
@@ -11,3 +16,11 @@ arithmaticOperation[result1]=$result1
 arithmaticOperation[result2]=$result2
 arithmaticOperation[result3]=$result3
 arithmaticOperation[result4]=$result4
+#read the value dict to array
+
+for((i=0; i<=${#arithmaticOperation[@]}; i++))
+do
+  array[i]=${arithmaticOperation[result$((i+1))]}
+done
+#TO PRINT
+echo "${array[@]}"
