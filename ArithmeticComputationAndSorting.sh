f92bffd5a3243ab4e92772aa8b6f89de8fb501c3
bhushan_patil <bhushan>
2021-01-08 23:15:31 +0530

    sort result show desending order

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -24,3 +24,7 @@ do
 done
 #TO PRINT
 echo "${array[@]}"
+echo "${array[@]}"
+echo "The computation expressions result in Descending"
+sortedDesc=($(echo ${array[@]}| tr " " "\n" | sort -nr))
+echo ${sortedDesc[@]}
